9ca9fde163f78e0115da2ae8ba25886fdce6b03d
Lovel Piližota <lovel.pilizota@gmail.com>
2022-11-04 12:35:28 +0100

    Initial commit

---
+++ src/test/java/hr/tvz/pilizota/managerapp/EmployeeManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.tvz.pilizota.managerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
